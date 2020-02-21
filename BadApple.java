687673cf85b95705fd1b7c4f35a5349228d52096
dliust <dliust16@gmail.com>
2020-02-21 13:23:30 -0800

    Add files via upload
    Might want to remove bad apple

---
+++ BadApple.java
@@ -0,0 +1,17 @@
+package com.gamecodeschool.c17snake;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Point;
+import java.util.Random;
+
+public class BadApple extends Apple {
+    BadApple(Context context, Point sr, int s) {
+        super(context, sr, s);
+    }
+
+    //change draw method with new png file
+}
